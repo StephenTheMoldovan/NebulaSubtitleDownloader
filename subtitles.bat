01faac861a375d812aec21c3561451fc8cc4d3ef
StephenTheMoldovan <57498947+StephenTheMoldovan@users.noreply.github.com>
2023-01-01 21:43:21 +0200

    Update subtitles.bat

--- subtitles.bat
+++ subtitles.bat
@@ -1,9 +1,9 @@
 @echo off
 title Subtitle Downloader and Merger for Nebula
 echo I hope you edited the code to include the link to the first segment and the next ones!
-wget --no-check-certificate https://storage.googleapis.com/zype-subtitles-prod/5c182d06649f0f134a001703/5fd838c1af68b90001c60431/subtitles/sub_en_0.vtt
-for /l %%x in (1, 1, 200) do (
-wget --no-check-certificate https://storage.googleapis.com/zype-subtitles-prod/5c182d06649f0f134a001703/5fd838c1af68b90001c60431/subtitles/sub_en_%%x.vtt
+wget --no-check-certificate a./subtitles/sub_en_0.vtt
+for /l %%x in (1, 1, b) do (
+wget --no-check-certificate a./subtitles/sub_en_%%x.vtt
 )
 copy sub_en_*.vtt sub_en.vtt
 pause
